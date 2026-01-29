ccd752a773a5ea11ce5a73a9ad5e4d93fd5253ee
Kartik Bhangale <kartikbhangale20@gmail.com>
2026-01-29 19:39:16 +0530

    Time: 0 ms (100.00%) | Memory: 35.6 MB (78.21%) - LeetSync

---
+++ 724-find-pivot-index/find-pivot-index.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int pivotIndex(vector<int>& nums) {
+        int total=0;
+        for(int x : nums){
+            total+=x;
+        }
+        int prefix=0;
+        for(int i=0;i<nums.size();i++){
+            int leftsum=prefix;
+            int rightsum=total-leftsum-nums[i];
+            if(leftsum==rightsum) return i;
+            prefix+=nums[i];
+        }
+        return -1;
+    }
+};
