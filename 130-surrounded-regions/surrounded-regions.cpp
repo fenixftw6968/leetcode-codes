81b3dd8cadb68970d43495f464f8cea99005bf89
Kartik Bhangale <kartikbhangale20@gmail.com>
2026-04-07 18:09:11 +0530

    Time: 0 ms (100.00%) | Memory: 14 MB (80.06%) - LeetSync

---
+++ 130-surrounded-regions/surrounded-regions.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    void dfs(vector<vector<char>>& board, int i, int j) {
+        if (i < 0 || i >= board.size() || j < 0 || j >= board[0].size() || board[i][j] != 'O') {
+            return;
+        }
+        board[i][j] = 'T'; //safe
+
+        dfs(board, i + 1, j);
+        dfs(board, i - 1, j);
+        dfs(board, i, j + 1);
+        dfs(board, i, j - 1);
+    }
+    void solve(vector<vector<char>>& board) {
+        if (board.empty() || board[0].empty()) return;
+        int m = board.size(), n = board[0].size();
+        for(int i=0;i<m;i++){
+            if(board[i][0]=='O') dfs(board,i,0);
+            if(board[i][n-1]=='O') dfs(board,i,n-1);
+        }
+        for(int i=0;i<n;i++){
+            if(board[0][i]=='O') dfs(board,0,i);
+            if(board[m-1][i]=='O') dfs(board,m-1,i);
+        }
+
+
+        //final
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(board[i][j]=='O') board[i][j]='X';
+                else if(board[i][j]=='T') board[i][j]='O';
+            }
+        }
+    }
+};
