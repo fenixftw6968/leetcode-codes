90d695cc0b4b4b042c1d9f43c44dd2df6f03d220
Kartik Bhangale <kartikbhangale20@gmail.com>
2026-04-13 21:00:54 +0530

    Time: 25 ms (20.48%) | Memory: 110.1 MB (17.57%) - LeetSync

---
+++ 2764-maximum-number-of-fish-in-a-grid/maximum-number-of-fish-in-a-grid.cpp
@@ -0,0 +1,45 @@
+class Solution {
+private:
+    int islandPerimeter(vector<vector<int>> &grid,vector<vector<int>> &vis,int row,int col) {
+        int n=grid.size();
+        int m=grid[0].size();
+        bool flag=true;
+        queue<pair<int,int>> q;
+        q.push({row,col});
+        vis[row][col]=1;
+        int dcols[]={1,0,-1,0};
+        int drows[]={0,-1,0,1};
+        int area=grid[row][col];
+        while(!q.empty()){
+            auto top=q.front();
+            q.pop();
+            int r=top.first;
+            int c=top.second;
+            for(int i=0;i<4;i++){
+                int nrow=r+drows[i];
+                int ncol=c+dcols[i];
+                if(nrow>=0 && nrow<n && ncol>=0 && ncol<m && grid[nrow][ncol]>0 && vis[nrow][ncol]==0){
+                    area+=grid[nrow][ncol];
+                    q.push({nrow,ncol});
+                    vis[nrow][ncol]=1;
+                }
+            }
+        }
+        return area;
+    }
+public:
+    int findMaxFish(vector<vector<int>>& grid) {
+        int n=grid.size();
+        int m=grid[0].size();
+        int maxi=0;
+        vector<vector<int>> vis(n,vector<int> (m,0));
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(grid[i][j]>0 && vis[i][j]==0){
+                    maxi=max(maxi,islandPerimeter(grid,vis,i,j));
+                }
+            }
+        }
+        return maxi;
+    }
+};
